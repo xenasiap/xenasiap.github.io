85eaa8d67fcb660f830b49e957adb3d53e593c18
Xena Siapkara <siapzena@gmail.com>
2021-10-26 23:40:22 +0300

    Changed the speed of the header

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
             strings: typed_strings.split(', '),
-            typeSpeed: 100,
+            typeSpeed: 50,
             backSpeed: 20,
             smartBackspace: false,
             loop: true
